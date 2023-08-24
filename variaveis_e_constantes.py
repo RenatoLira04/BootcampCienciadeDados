3d334031adf7589208d27c07a6d62e471ef4acb1
Renato Lira <82622095+RenatoLira04@users.noreply.github.com>
2023-08-23 23:15:03 -0300

    variaveis e constantes
    exercicio de variaveis e constantes

---
+++ variaveis_e_constantes.py
@@ -0,0 +1,8 @@
+nome = "Renato"
+idade = 34
+nome, idade = "Marcos", 41
+print(nome, idade)
+limite_saque_diario = 1000
+BRAZILIAN_STATES = ["SP", "RJ", "MG", "BA"]
+BRAZILIAN_STATES = 18
+print(BRAZILIAN_STATES)
