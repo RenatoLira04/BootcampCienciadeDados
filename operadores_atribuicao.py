65dea13a3222c1af6c4e5ccd101a8b753a82eb65
Renato Lira <82622095+RenatoLira04@users.noreply.github.com>
2023-08-25 23:11:40 -0300

    operaddores de atribuição.
    exercício de python utilizando operadores de atribuição.

---
+++ operadores_atribuicao.py
@@ -0,0 +1,23 @@
+saldo = 500
+print(saldo)
+saldo0 = 500
+saldo0 += 200
+print(saldo0)
+saldo1 = 500
+saldo1 -= 100
+print(saldo1)
+saldo2 = 500
+saldo2 *=2
+print(saldo2)
+saldo3 = 500
+saldo3 /=5
+print(saldo3)
+saldo4 = 500
+saldo4 //= 5
+print(saldo4)
+saldo5 = 500
+saldo5 %= 480
+print(saldo5)
+saldo6 = 80
+saldo6 **= 2
+print(saldo6)
