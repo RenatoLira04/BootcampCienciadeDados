7c4bbe21cb4a33bee98f5e1bb62aa06be7f85c6b
Renato Lira <82622095+RenatoLira04@users.noreply.github.com>
2023-08-28 15:48:45 -0300

    operadores de identidade.
    exercício de operadores de identidade no python

---
+++ operadores_identidade.py
@@ -0,0 +1,16 @@
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 200, 200
+print(curso)
+print(nome_curso)
+print(saldo)
+print(limite)
+
+v = curso is nome_curso
+print(v)
+
+v1 = curso is not nome_curso
+print(v1)
+
+v2 = saldo is limite
+print(v2)
