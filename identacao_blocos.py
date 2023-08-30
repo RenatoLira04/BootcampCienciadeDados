e7f70d222224239741529ba655031f09faddba4b
Renato Lira <82622095+RenatoLira04@users.noreply.github.com>
2023-08-29 23:19:22 -0300

    identação e blocos
    exercício de identeção e blocos  utilizando python.

---
+++ identacao_blocos.py
@@ -0,0 +1,9 @@
+def sacar(valor):
+    saldo = 500
+
+    if saldo >= valor:
+        print("valor sacado")
+        print("Retire seu dinheiro na boca do caixa.")
+    print("Obrigado por ser nosso cliente, tenha um bom dia!")
+
+sacar(1000)
