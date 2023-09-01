a3ad1c63f1a5475de068f055d384a3dd9cbb422e
Renato Lira <82622095+RenatoLira04@users.noreply.github.com>
2023-09-01 07:41:40 -0300

    trabalhando con string part 1
    exercício de manipulação de string part 1 em python.

---
+++ string_1.py
@@ -0,0 +1,13 @@
+curso = "pYtHon"
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+
+curso1 = "   Python "
+print(curso1.strip() + ".")
+print(curso1.lstrip() + ".")
+print(curso1.rstrip() + ".")
+
+curso2= "Python"
+print(curso2.center(10, "#"))
+print(".".join(curso2))
